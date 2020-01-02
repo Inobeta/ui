924baf4e8ff21cc883107cef0e3e954301f86273
Luca Casamenti <luca.casamenti@inobeta.net>
2020-01-02 12:11:33 +0100

    arricchito stub del TranslateService

--- src/app/inobeta-ui/stubs/translate.service.stub.ts
+++ src/app/inobeta-ui/stubs/translate.service.stub.ts
@@ -3,5 +3,11 @@ import {of} from 'rxjs';
 export const TranslateServiceStub = {
   get(key: any): any {
     return of(key);
+  },
+  setDefaultLang() {
+    return true;
+  },
+  use() {
+    return of({});
   }
 };
