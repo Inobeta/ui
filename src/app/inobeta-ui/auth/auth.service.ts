c5344074533bd1c33c0a069d3e4a8bbd8baea98a
salvo <salvatore.niglio@inobeta.net>
2020-11-02 18:31:11 +0100

    eliminato un merdosissimo bug maledetto

--- src/app/inobeta-ui/auth/auth.service.ts
+++ src/app/inobeta-ui/auth/auth.service.ts
@@ -19,9 +19,9 @@ export class AuthService {
     if (!this.activeSession) {
       this.activeSession = this.srvLocalStorage.get(`userData-${this.sessionStorageKey}`) as Session;
     }
-    if (!this.activeSession) {
+    /*if (!this.activeSession) {
       this.store.dispatch(SessionActions.logout());
-    }
+    }*/
   }
 
   public storeSession() {
