65d2198ef3046ce544cbaa2be0737e43aec6fcd6
salvo <salvatore.niglio@inobeta.net>
2021-03-10 15:10:22 +0100

    ci enabled for examples deploy

--- src/app/examples/toast-example/toast-example.component.ts
+++ src/app/examples/toast-example/toast-example.component.ts
@@ -15,7 +15,7 @@ import { IbToastNotification } from 'src/app/inobeta-ui/ui/toast/toast.service';
 export class IbToastExampleComponent {
   constructor(private toast: IbToastNotification) { }
 
-  open(type){
+  open(type = 'success'){
     this.toast.open('examples.toastMessage', type)
   }
 }
