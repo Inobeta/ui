91ae2733a974688e38b63f8315d2b6748066f109
salvo <salvatore.niglio@inobeta.net>
2021-04-01 09:15:57 +0200

    table stub fix

--- src/app/inobeta-ui/ui/table/table.stub.spec.ts
+++ src/app/inobeta-ui/ui/table/table.stub.spec.ts
@@ -1,32 +1,41 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { IbTableTitles } from './models/titles.model';
+import { IbTableAction, IbTableTitles } from './models/titles.model';
 import { IbTemplateModel } from './models/template.model';
+import { IbTableItem } from './models/table-item.model';
+import { jsPDFOptions } from 'jspdf';
 
 @Component({
   selector: 'ib-table',
   template: ``,
 })
 export class IbTableStubComponent {
+  @Input() customItemTemplate: any;
   @Input() titles: IbTableTitles[] = [];
-  @Input() items: any[] = [];
-  @Input() filterValues: any = {};
+  @Input() items: IbTableItem[] = [];
+  @Input() enableReduxStore = false;
   @Input() currentSort: any = {};
   @Input() selectableRows = true;
   @Input() hasAdd = false;
-  @Input() hasFilterReset = false;
-  @Input() hasSearch = false;
+  @Input() hasEdit = false;
+  @Input() hasDelete = false;
   @Input() hasExport = false;
   @Input() hasPaginator = true;
-  @Input() structureTemplates;
-  @Input() hasActions = false;
+  @Input() actions: IbTableAction[] = [];
+  @Input() structureTemplates = {};
   @Input() templateButtons: IbTemplateModel[] = [];
   @Input() templateHeaders: any = {};
-  @Input() actions: string[] = [];
+  @Input() tableName = 'default_table_name';
+  @Input() pdfCustomStyles = {};
+  @Input() pdfSetup: jsPDFOptions = {
+    orientation: 'l',
+    unit: null,
+    format: null
+  };
   @Output() filterChange: EventEmitter<any> = new EventEmitter<any>();
-  @Output() filterReset: EventEmitter<any> = new EventEmitter<any>();
   @Output() sortChange: EventEmitter<any> = new EventEmitter<any>();
   @Output() add: EventEmitter<any> = new EventEmitter<any>();
-  @Output() deleteItem: EventEmitter<any> = new EventEmitter<any>();
-  @Output() arrowClick: EventEmitter<any> = new EventEmitter<any>();
-  @Output() actionsClick: EventEmitter<any> = new EventEmitter<any>();
+  @Output() edit: EventEmitter<any> = new EventEmitter<any>();
+  @Output() delete: EventEmitter<any> = new EventEmitter<any>();
+  @Output() rowClicked: EventEmitter<any> = new EventEmitter<any>();
+  @Output() rowChecked: EventEmitter<any> = new EventEmitter<any>();
 }
