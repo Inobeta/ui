fe61f2ecf3d5198246c830ebc7f1d5da0bb10b3a
Luca Palumbo <luca.palumbo@inobeta.net>
2021-08-25 10:54:24 +0200

    rafactor format function

--- src/app/inobeta-ui/ui/table/store/reducers/table.reducer.ts
+++ src/app/inobeta-ui/ui/table/store/reducers/table.reducer.ts
@@ -27,10 +27,10 @@ export interface IbTablePaginatorState {
 }
 
 export interface IbTableConfigState {
-  filters: IbTableFilterState[];
-  totals: IbTableTotalRowState[];
-  sort: IbTableSortState;
-  paginator: IbTablePaginatorState;
+  filters?: IbTableFilterState[];
+  totals?: IbTableTotalRowState[];
+  sort?: IbTableSortState;
+  paginator?: IbTablePaginatorState;
   default?: boolean;
 }
 
@@ -52,32 +52,38 @@ const reducer = createReducer(
 
   on(TableActions.ibTableActionSetTotalRowCell,
     (state, newTotalRowState) => {
-      if (!newTotalRowState.tableName){
+      if (!newTotalRowState.tableName) {
         return { ...state };
       }
       const instance = state.instances?.find(i => i.tableName === newTotalRowState.tableName);
-      if (!instance){
-        return formatFieldState(state, newTotalRowState.tableName, null, [newTotalRowState.state]);
+      if (!instance) {
+        return formatFieldState(state, newTotalRowState.tableName, null, { totals: [newTotalRowState.state] });
       }
       state.instances.splice(state.instances.indexOf(instance), 1);
       const rowExists = instance.config?.totals.find(t => t.columnName === newTotalRowState.state.columnName);
       if (!rowExists) {
-        return formatFieldState(state, newTotalRowState.tableName, instance, [...(instance.config?.totals || []), newTotalRowState.state]);
+        return formatFieldState(state,
+          newTotalRowState.tableName,
+          instance,
+          { totals: [...(instance.config?.totals || []), newTotalRowState.state] });
       }
-      return formatFieldState(state, newTotalRowState.tableName, instance, instance.config.totals.map(
-        t => t.columnName === newTotalRowState.state.columnName
-          ? ({ columnName: newTotalRowState.state.columnName, func: newTotalRowState.state.func })
-          : t
-      ));
+      return formatFieldState(state, newTotalRowState.tableName, instance,
+        {
+          totals: instance.config.totals.map(
+            t => t.columnName === newTotalRowState.state.columnName
+              ? ({ columnName: newTotalRowState.state.columnName, func: newTotalRowState.state.func })
+              : t
+          )
+        });
     }
   ),
 
   on(TableActions.ibTableActionSetConfig, (state, newConfigData) => {
-    if (!newConfigData.config){
-      return {...state};
+    if (!newConfigData.config) {
+      return { ...state };
     }
     const instance = state.instances?.find(i => i.tableName === newConfigData.tableName);
-    if (instance){
+    if (instance) {
       state.instances.splice(state.instances.indexOf(instance), 1);
     }
     return {
@@ -94,28 +100,28 @@ const reducer = createReducer(
   }),
 
   on(TableActions.ibTableActionSaveConfig, (state, saveConfigData) => {
-    return {...state, selectedConfig: saveConfigData.options.data.name};
+    return { ...state, selectedConfig: saveConfigData.options.data.name };
   }),
 
   on(TableActions.ibTableActionSelectSortingField, (state, sortData) => {
-    if (!sortData.tableName){
+    if (!sortData.tableName) {
       return { ...state };
     }
     const instance = state.instances?.find(i => i.tableName === sortData.tableName);
-    if (instance){
+    if (instance) {
       state.instances.splice(state.instances.indexOf(instance), 1);
     }
-    return formatFieldState(state, sortData.tableName, instance, null, sortData.options);
+    return formatFieldState(state, sortData.tableName, instance, { sort: sortData.options });
   }),
 
   on(TableActions.ibTableActionAddFilterField,
     (state, addFilterState) => {
-      if (!addFilterState.tableName){
+      if (!addFilterState.tableName) {
         return { ...state };
       }
       const instance = state.instances?.find(i => i.tableName === addFilterState.tableName);
-      if (!instance){
-        return formatFieldState(state, addFilterState.tableName, null, null, [addFilterState.state]);
+      if (!instance) {
+        return formatFieldState(state, addFilterState.tableName, null, { filters: [addFilterState.state] });
       }
       state.instances.splice(state.instances.indexOf(instance), 1);
       const rowExists = instance.config?.filters.find(t => t.columnName === addFilterState.state.columnName);
@@ -123,27 +129,27 @@ const reducer = createReducer(
         return formatFieldState(state,
           addFilterState.tableName,
           instance,
-          null,
-          null,
-          [...(instance.config?.filters || []), addFilterState.state]);
+          { filters: [...(instance.config?.filters || []), addFilterState.state] });
       }
-      return formatFieldState(state, addFilterState.tableName, instance, null, null, instance.config.filters.map(
-        t => t.columnName === addFilterState.state.columnName
-          ? ({ columnName: addFilterState.state.columnName, value: addFilterState.state.value })
-          : t
-      ));
+      return formatFieldState(state, addFilterState.tableName, instance, {
+        filters: instance.config.filters.map(
+          t => t.columnName === addFilterState.state.columnName
+            ? ({ columnName: addFilterState.state.columnName, value: addFilterState.state.value })
+            : t
+        )
+      });
     }
   ),
 
   on(TableActions.ibTableActionSetPaginator, (state, paginatorData) => {
-    if (!paginatorData.tableName){
+    if (!paginatorData.tableName) {
       return { ...state };
     }
     const instance = state.instances?.find(i => i.tableName === paginatorData.tableName);
-    if (instance){
+    if (instance) {
       state.instances.splice(state.instances.indexOf(instance), 1);
     }
-    return formatFieldState(state, paginatorData.tableName, instance, null, null, null, paginatorData.state);
+    return formatFieldState(state, paginatorData.tableName, instance, { paginator: paginatorData.state });
   }),
 );
 
@@ -153,21 +159,18 @@ export function ibTableFeatureReducer(state: IbTableState = ibTableFeatureInitia
 }
 
 
-function formatFieldState(state, tableName, instance, totals?, sort?, filters?, paginator?): IbTableState{
+function formatFieldState(state, tableName, instance, newConfig: IbTableConfigState): IbTableState {
 
+  const instanceConfig: { config: IbTableConfigState, tableName } =
+    { config: { totals: [], sort: null, filters: [], paginator: null }, tableName };
+  for (const k of Object.keys(instanceConfig.config)) {
+    instanceConfig.config[k] = !newConfig[k] ? instance?.config[k] || instanceConfig.config[k] : newConfig[k] || instanceConfig.config[k];
+  }
   return {
     ...state,
     instances: [
       ...(state.instances || []),
-      {
-        tableName,
-        config: {
-          filters: totals || sort || paginator ? instance?.config.filters || [] : filters || [],
-          sort: totals || filters || paginator ? instance?.config.sort || [] : sort || {},
-          totals: filters || sort || paginator ? instance?.config.totals || [] : totals || [],
-          paginator: filters || sort || totals ? instance?.config.paginator || [] : paginator || {},
-        }
-      }
+      instanceConfig
     ]
   };
 }
