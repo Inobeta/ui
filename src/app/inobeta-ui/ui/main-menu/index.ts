d840937acec314128b495f6d5ef4a30d231c4865
robspe <roberto.specchia@inobeta.net>
2021-09-28 18:16:10 +0200

    DEV-149 uodate index.ts

--- src/app/inobeta-ui/ui/main-menu/index.ts
+++ src/app/inobeta-ui/ui/main-menu/index.ts
@@ -1,2 +1,4 @@
 export * from './main-menu.module';
 export * from './components/main-menu-bar/main-menu-bar.component'
+export * from './models/main-menu-button.model'
+export * from './models/main-menu-data.model'
