3dd689c541dc3287640fb0c79e53534d3cc20b36
Salvatore Niglio <salvatore.niglio@inobeta.net>
2023-01-10 18:49:21 +0100

    DEVK-169 ugly but it works

--- src/app/inobeta-ui/ui/material-forms/controls/textbox.ts
+++ src/app/inobeta-ui/ui/material-forms/controls/textbox.ts
@@ -102,9 +102,24 @@ export class IbMatTextboxComponent implements IbFormControlInterface {
 
   get maxLengthValidator() {
     for (const func of this.data.base.validators) {
-      const validation = func({value: {
-        length: Infinity
-      }} as AbstractControl);
+      const getMethods = (obj) => {
+        let properties = new Set()
+        let currentObj = obj
+        do {
+          Object.getOwnPropertyNames(currentObj).map(item => properties.add(item))
+        } while ((currentObj = Object.getPrototypeOf(currentObj)))
+        return [...properties.keys()].filter((item: any) => typeof obj[item] === 'function') as string[]
+      }
+      const sampleString = this.data.self.value
+      if(!sampleString) return Infinity
+
+      const methods: string[] = getMethods(sampleString)
+      const fakeString: any = {}
+      for(let m of methods){
+        fakeString[m] = sampleString[m].bind(sampleString)
+      }
+      fakeString['length'] = Infinity
+      const validation = func({value: fakeString} as AbstractControl);
       if (validation && validation.maxlength) {
         return validation.maxlength.requiredLength;
       }
