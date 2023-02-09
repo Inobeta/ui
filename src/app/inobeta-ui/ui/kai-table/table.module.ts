5a6d2d7adcb6582b369ecc43abf36c6294d7db3b
Salvatore Niglio <salvatore.niglio@inobeta.net>
2023-02-09 12:57:23 +0100

    DEVK-183 paginator translations

--- src/app/inobeta-ui/ui/kai-table/table.module.ts
+++ src/app/inobeta-ui/ui/kai-table/table.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { IbTable } from './table.component';
 import { PortalModule } from '@angular/cdk/portal';
 import { MatSortModule } from '@angular/material/sort';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { MatIconModule } from '@angular/material/icon';
-import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatPaginatorIntl, MatPaginatorModule } from '@angular/material/paginator';
 import { IbContextActionCell } from './cells';
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
+//FIXME: please move this into kai-table
+import { ibMatPaginatorTranslate } from '../table';
 
 @NgModule({
   exports: [
@@ -29,6 +31,11 @@ import { MatButtonModule } from '@angular/material/button';
     TranslateModule.forChild({
       extend: true
     })
-  ]
+  ],
+  providers: [
+    {
+      provide: MatPaginatorIntl, deps: [TranslateService],
+      useFactory: ibMatPaginatorTranslate
+    }]
 })
 export class IbKaiTableModule { }
