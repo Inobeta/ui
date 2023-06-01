76513eb902e9812b2b33bff44615274f24c5c6aa
jacopo.locascio <jacopo.locascio@inobeta.net>
2023-06-01 16:53:18 +0200

    DEVK-184: bug(IbFilter): export module

--- public_api.ts
+++ public_api.ts
@@ -11,5 +11,6 @@ export * from './src/app/inobeta-ui/storage/index';
 export * from './src/app/inobeta-ui/tools/index';
 export * from './src/app/inobeta-ui/hydration/index';
 export * from './src/app/inobeta-ui/ui/kai-table/index';
+export * from './src/app/inobeta-ui/ui/kai-filter/index';
 
 
