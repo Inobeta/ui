482db838742a8fd7bce47f9060b11a7b8f9411af
jacopo.locascio <jacopo.locascio@inobeta.net>
2023-08-17 11:51:34 +0200

    DEVK-186 fix(IbFilter): align action buttons to end

--- src/app/inobeta-ui/ui/kai-filter/filter-actions/filter-actions.component.ts
+++ src/app/inobeta-ui/ui/kai-filter/filter-actions/filter-actions.component.ts
@@ -4,7 +4,9 @@ import { Component } from "@angular/core";
   selector: "ib-filter-actions",
   template: `
     <mat-divider></mat-divider>
-    <section style="margin-left: 1em; margin-top: 0.5em">
+    <section
+      style="display:flex; justify-content: flex-end; margin-right: 1em; margin-top: 0.5em"
+    >
       <ng-content></ng-content>
     </section>
   `,
