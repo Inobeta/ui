897ac040b1c43ab65bdb09f669868042e6c9606a
Salvatore Niglio <salvatore.niglio@inobeta.net>
2023-11-20 18:52:25 +0100

    DEVK-206 auth interceptor

--- src/app/inobeta-ui/http/http/auth.interceptor.ts
+++ src/app/inobeta-ui/http/http/auth.interceptor.ts
@@ -1,33 +1,51 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Inject, Injectable, Optional } from '@angular/core';
-import { Router } from '@angular/router';
 import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 import { IbToastNotification } from '../../ui/toast/toast.service';
-import { IbSessionService } from '../auth/session.service';
+import { IbAuthService } from '../auth/auth.service';
+import { IbLoginService } from '../auth/login.service';
+import { IbAPITokens, IbAuthTypes } from '../auth/session.model';
 
 @Injectable({providedIn: 'root'})
 export class IbAuthInterceptor implements HttpInterceptor {
   constructor(
-    private router: Router,
+    private auth: IbAuthService<IbAPITokens>,
     private ibToast: IbToastNotification,
-    private session: IbSessionService,
+    private login: IbLoginService<IbAPITokens>,
     @Inject('ibHttpEnableInterceptors') @Optional() public ibHttpEnableInterceptors?: boolean,
     @Inject('ibHttpAPILoginUrl') @Optional() public ibHttpAPILoginUrl?: string,
     @Inject('ibHttpToastOnLoginFailure') @Optional() public ibHttpToastOnLoginFailure?: string,
+    @Inject('ibHttpAuthType') @Optional() public ibHttpAuthType?: IbAuthTypes,
   ) {
     this.ibHttpEnableInterceptors = this.ibHttpEnableInterceptors === null ? true : false;
     this.ibHttpAPILoginUrl = this.ibHttpAPILoginUrl || '/api/login';
     this.ibHttpToastOnLoginFailure = this.ibHttpToastOnLoginFailure || 'shared.ibHttp.authFailure';
   }
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return next.handle(request).pipe(catchError(err => {
+    let authString = ''
+
+    let authReq = request
+    switch(this.ibHttpAuthType){
+      case IbAuthTypes.JWT: authString = 'Bearer '; break;
+      case IbAuthTypes.BASIC_AUTH: authString = 'Basic '; break;
+    }
+    if(this.auth.activeSession.valid && this.auth.activeSession.serverData.accessToken){
+      authString += this.auth.activeSession.serverData.accessToken
+      authReq = request.clone({
+        headers: request.headers
+          .set('Authorization', authString)
+          .set('Content-Type', 'application/json')
+          .set('x-requested-with', 'XMLHttpRequest')
+      })
+    }
+    return next.handle(authReq).pipe(catchError(err => {
       if (!this.ibHttpEnableInterceptors) {
         return throwError(err);
       }
       if (request.url !== this.ibHttpAPILoginUrl && err.status === 401) {
         this.ibToast.open(this.ibHttpToastOnLoginFailure, 'warning');
-        this.session.logout(false);
+        this.login.logout(false);
       }
       return throwError(err);
     }));
