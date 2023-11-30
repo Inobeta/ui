c3e40753c25745a8b9bba79f45f910f6964d4178
Salvatore Niglio <salvatore.niglio@inobeta.net>
2023-11-30 12:07:29 +0100

    DEVK-206 refresh cycle draft for test

--- src/app/inobeta-ui/http/store/session/effects.ts
+++ src/app/inobeta-ui/http/store/session/effects.ts
@@ -1,31 +1,72 @@
-import { Injectable } from "@angular/core";
+import { Inject, Injectable, Optional } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { ibAuthActions } from "./actions";
-import { of } from "rxjs";
-import { mergeMap, delay, switchMap, catchError } from "rxjs/operators";
-import { IbSession } from "../../auth/session.model";
+import { combineLatest, of } from "rxjs";
+import { mergeMap, delay, switchMap, catchError, withLatestFrom, map, filter } from "rxjs/operators";
+import { IbAPITokens, IbSession } from "../../auth/session.model";
+import { Store } from "@ngrx/store";
+import { ibSelectAccessTokenExp, ibSelectActiveSession } from "./selectors";
+import { IbLoginService } from "../../auth/login.service";
+import { IbAuthService } from "../../auth/auth.service";
+import { IbStorageTypes } from "src/app/inobeta-ui/storage/storage.service";
 
 @Injectable({providedIn: 'root'})
 export class IbSessionEffects{
+
+  login$ = createEffect(() => {
+    return this.actions$.pipe(
+        ofType(ibAuthActions.login),
+        map((action) => {
+          this.authLegacy.activeSession = action.activeSession;
+          if (this.ibHttpSessionStorageType === IbStorageTypes.LOCALSTORAGE) {
+            this.authLegacy.storeSession();
+          } else {
+            this.authLegacy.cookieSession();
+          }
+        })
+      ).pipe(
+        switchMap(() => [ibAuthActions.refreshCycle()])
+      )
+  });
+
+
+
   refreshCycle$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(ibAuthActions.refreshCycle),
-      mergeMap((action) => of(action).pipe(
-        delay(this.csmsAuth.getRefreshTimer())
+      withLatestFrom(combineLatest([
+        this.store.select(ibSelectActiveSession<IbAPITokens>()),
+        this.store.select(ibSelectAccessTokenExp)
+      ])),
+      filter(([, [session]]) => session !== null),
+      mergeMap(([, [session, exp]]) => of(session).pipe(
+        delay(exp)
       )),
-      mergeMap(() => this.csmsAuth.tokenRefresh()),
-      mergeMap((session: ILoginData) => [this.csmsAuth.storeFreshSession(session)]),
+      mergeMap((session) => this.login.refresh(session.serverData.refreshToken)),
     ).pipe(
-      switchMap((newSession: IbSession<ILoginData>) => [
-        authActions.resetSession({newSession})
-      ]),
+      switchMap((serverData: IbAPITokens) => {
+        const activeSession =  new IbSession<IbAPITokens>();
+        activeSession.user.password = '';
+        activeSession.valid = true;
+        activeSession.serverData = serverData;
+        return [
+          ibAuthActions.login({activeSession})
+        ]
+      }),
       catchError(() => [ibAuthActions.logout()]),
     )
   });
 
+
   constructor(
-    private actions$: Actions
-  ) { }
+    private actions$: Actions,
+    private store: Store,
+    private login: IbLoginService<IbAPITokens>,
+    private authLegacy: IbAuthService<IbAPITokens>,
+    @Inject('ibHttpSessionStorageType') @Optional() public ibHttpSessionStorageType?: IbStorageTypes,
+  ) {
+    this.ibHttpSessionStorageType = this.ibHttpSessionStorageType ?? IbStorageTypes.LOCALSTORAGE;
+  }
 
 
 }
--- src/app/inobeta-ui/http/store/session/selectors.ts
+++ src/app/inobeta-ui/http/store/session/selectors.ts
@@ -1,4 +1,5 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { jwtDecode } from 'jwt-decode';
 import { IHttpStore } from '..';
 import { IbAPITokens, IbSession } from '../../auth/session.model';
 import { httpFeatureKey } from '../const';
@@ -7,5 +8,19 @@ const selectFeature = createFeatureSelector<IHttpStore>(httpFeatureKey);
 
 export const ibSelectActiveSession = <T extends IbAPITokens | IbAPITokens>() => createSelector(
   selectFeature,
-  (state: IHttpStore): IbSession<T> => state?.session?.activeSession as IbSession<T>
+  (state: IHttpStore): IbSession<T> | null => (state?.session?.activeSession as IbSession<T>) ?? null
+)
+
+export const ibSelectAccessTokenExp = createSelector(
+  selectFeature,
+  (state: IHttpStore): number => {
+    const token = state.session.activeSession?.serverData?.accessToken
+    if(!token) return 0
+    const decoded: {
+      exp: number
+    } = jwtDecode(state.session.activeSession.serverData.accessToken);
+    const now = new Date().getTime() / 1000;
+    return  (decoded.exp - now - 3600) * 1000
+
+  }
 )
