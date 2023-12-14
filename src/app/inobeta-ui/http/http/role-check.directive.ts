76d5e587519e8a2a20edb24e4e5dd08eff8d731b
Salvatore Niglio <salvatore.niglio@inobeta.net>
2023-12-14 18:52:44 +0100

    DEVK-9 directive & guard for role check

---
+++ src/app/inobeta-ui/http/http/role-check.directive.ts
@@ -0,0 +1,34 @@
+import { Directive, Input, OnDestroy, OnInit, TemplateRef, ViewContainerRef } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { IbAPITokens } from '../auth/session.model';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+import { IbLoginService } from '../auth/login.service';
+
+@Directive({ selector: '[ibRoleCheck]' })
+export class IbRoleCheckDirective implements OnInit, OnDestroy{
+  @Input('ibRoleCheck') roles: string[]
+  destroy: Subject<boolean> = new Subject()
+
+
+  constructor(private tpl: TemplateRef<any>,
+    private vcr: ViewContainerRef,
+    private store: Store,
+    private login: IbLoginService<IbAPITokens>
+    ) {
+    }
+  ngOnDestroy(): void {
+    this.destroy.next()
+    this.destroy.complete()
+  }
+  ngOnInit(): void {
+    this.login.hasRoles(this.roles)
+    .pipe(takeUntil(this.destroy))
+    .subscribe(hasRole => {
+      this.vcr.clear();
+      if(hasRole){
+        this.vcr.createEmbeddedView(this.tpl);
+      }
+    })
+  }
+}
