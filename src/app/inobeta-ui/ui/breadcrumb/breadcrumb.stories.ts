aaabf2a1af62052b17b3b14eefe484780732626e
Jacopo <jacopo.locascio@inobeta.net>
2024-01-31 15:32:44 +0100

    DEVK-272 docs(breadcrumb): add build your own section

--- src/app/inobeta-ui/ui/breadcrumb/breadcrumb.stories.ts
+++ src/app/inobeta-ui/ui/breadcrumb/breadcrumb.stories.ts
@@ -1,4 +1,7 @@
 import { StorybookTranslateModule } from ".storybook/i18n";
+import { NgFor, NgIf } from "@angular/common";
+import { Component } from "@angular/core";
+import { MatIconModule } from "@angular/material/icon";
 import { provideAnimations } from "@angular/platform-browser/animations";
 import { RouterTestingModule } from "@angular/router/testing";
 import {
@@ -7,17 +10,99 @@ import {
   applicationConfig,
   moduleMetadata,
 } from "@storybook/angular";
+import { IbBreadcrumbComponent } from "./breadcrumb.component";
 import { IbBreadcrumbModule } from "./breadcrumb.module";
-import { IbMaterialBreadcrumbComponent } from "./material-breadcrumb/material-breadcrumb.component";
 
-const meta: Meta<IbMaterialBreadcrumbComponent> = {
-  title: "Breadcrumbs",
-  component: IbMaterialBreadcrumbComponent,
+@Component({
+  selector: "app-breadbrumb",
+  standalone: true,
+  imports: [
+    NgIf,
+    NgFor,
+    StorybookTranslateModule,
+    RouterTestingModule,
+    IbBreadcrumbModule,
+    MatIconModule,
+  ],
+  template: `<ul class="ibBreadcrumbs">
+    <li *ngFor="let item of items" class="ibBreadcrumbs-link">
+      <p *ngIf="item === items[0]">
+        {{ item.label }}
+      </p>
+
+      <mat-icon *ngIf="item !== items[0] && item !== items[1]"
+        >chevron_right</mat-icon
+      >
+
+      <a *ngIf="item !== items[0]" [routerLink]="[item.url]">
+        {{ item.label }}
+      </a>
+    </li>
+  </ul>`,
+  styles: `
+  .ibBreadcrumbs {
+    padding: 5px 10px 5px 0px;
+    border-radius: 4px;
+    margin: 0px;
+    list-style: none;
+    display: flex;
+  }
+  .ibBreadcrumbs-link {
+    display: flex;
+    align-items: center;
+  }
+  .ibBreadcrumbs-link {
+    a {
+      background-color: transparent;
+      text-decoration: none;
+      line-height: 24px;
+      padding: 0px;
+      margin: 0px;
+      height: 33px;
+      font-weight: 500;
+      font-size: 14px;
+      line-height: 14px;
+      display: flex;
+      align-items: center;
+      text-align: center;
+      letter-spacing: 0.002em;
+      color: #030303;
+    }
+
+    a:hover {
+      text-decoration: underline;
+    }
+  }
+  .ibBreadcrumbs-link:nth-child(1) {
+    font-size: 20px;
+    line-height: 22px;
+    margin-right: 20px;
+    padding: 0px 5px 0px 10px;
+  }
+  .ibBreadcrumbs-link > mat-icon {
+    color: #A3A0A0;
+    margin: 0px;
+    height: 33px;
+    width: 24px;
+    text-align: center;
+    line-height: 35px;
+  }
+  `,
+  providers: [
+    { provide: IbBreadcrumbComponent, useExisting: CustomBreadcrumb },
+  ],
+})
+class CustomBreadcrumb extends IbBreadcrumbComponent {}
+
+const meta: Meta<CustomBreadcrumb> = {
+  title: "Components/Breadcrumbs",
+  component: CustomBreadcrumb,
   tags: ["autodocs"],
   decorators: [
     moduleMetadata({
       imports: [
         IbBreadcrumbModule,
+        CustomBreadcrumb,
         RouterTestingModule,
         StorybookTranslateModule,
       ],
@@ -34,14 +119,22 @@ const meta: Meta<IbMaterialBreadcrumbComponent> = {
 };
 
 export default meta;
-type Story = StoryObj<IbMaterialBreadcrumbComponent>;
+type Story = StoryObj<CustomBreadcrumb>;
 
-export const StaticMode: Story = {
+export const BuildYourOwn: Story = {
   args: {
     items: [
       {
-        url: "about-us",
-        label: "About us",
+        url: "#",
+        label: "Feature",
+      },
+      {
+        url: "#",
+        label: "section",
+      },
+      {
+        url: "#",
+        label: "page",
       },
     ],
     mode: "static",
