83096d04f833faeccaadba88ff70c75992397549
Jacopo <jacopo.locascio@inobeta.net>
2024-02-02 16:29:46 +0100

    DEVK-288 wip: docs

--- src/app/examples/kai-table-example/kai-table-example.ts
+++ src/app/examples/kai-table-example/kai-table-example.ts
@@ -1,38 +1,19 @@
-import { Component } from '@angular/core';
-import { IbTableDef } from '../../inobeta-ui/ui/kai-table/table.types';
-import { createNewUser } from './users';
+import { Component } from "@angular/core";
+import { IbTableDef } from "../../inobeta-ui/ui/kai-table/table.types";
+import { createNewUser } from "./users";
 
 @Component({
-  selector: 'ib-kai-table-example',
-  templateUrl: 'kai-table-example.html',
-  styles: [`
-  :host {
-    display: flex;
-    flex-direction: column;
-    padding: 30px;
-  }
-
-  ib-kai-table ::ng-deep .ib-cell-number{
-    text-align: right;
-    padding-right: 20px;
-  }
-
-  ib-kai-table ::ng-deep .ib-action-the-search-key{
-    color: purple;
-    font-weight: bold;
-  }
-
-  ib-kai-table ::ng-deep table th:first-of-type{
-    border-top-left-radius: 20px;
-  }
-
-  ib-kai-table ::ng-deep table th:last-of-type{
-    border-top-right-radius: 20px;
-  }
-  ib-kai-table ::ng-deep .ib-table-scrollable{
-    height: 450px;
-  }
-  `]
+  selector: "ib-kai-table-example",
+  templateUrl: "kai-table-example.html",
+  styles: [
+    `
+      :host {
+        display: flex;
+        flex-direction: column;
+        padding: 30px;
+      }
+    `,
+  ],
 })
 export class IbKaiTableExamplePage {
   data: any[];
@@ -42,16 +23,16 @@ export class IbKaiTableExamplePage {
       hide: true
     },*/
     initialSort: {
-      active: 'fruit',
-      direction: 'asc'
-    }
-  }
+      active: "fruit",
+      direction: "asc",
+    },
+  };
 
   ngOnInit() {
-    this.data = Array.from({ length: 1000 }, (_, k) => createNewUser(k + 1))
+    this.data = Array.from({ length: 1000 }, (_, k) => createNewUser(k + 1));
   }
 
-  testClick(ev){
-    console.log('ev', ev)
+  testClick(ev) {
+    console.log("ev", ev);
   }
 }
