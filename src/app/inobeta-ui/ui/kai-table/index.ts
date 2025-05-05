cfd1eb43bc1b0278c9ec4c23b7061ebf8b3e3b76
Jacopo <jacopo.locascio@inobeta.net>
2025-05-05 22:51:52 +0200

    fix(ui/table): missing exports for selectors

--- src/app/inobeta-ui/ui/kai-table/index.ts
+++ src/app/inobeta-ui/ui/kai-table/index.ts
@@ -1,12 +1,17 @@
 export * from "./action";
 export * from "./cells";
 export * from "./columns";
+export * from "./remote-data-source";
 export * from "./rowgroup";
 export * from "./sort-header";
-export * from "./remote-data-source";
+export {
+  ibTableSelectLastQueryString,
+  ibTableSelectLastQueryStringRaw,
+  ibTableSelectUrlState,
+} from "./store";
+export * from "./store/url-state/actions";
+export * from "./store/url-state/selectors";
+export * from "./table-url.service";
 export * from "./table.component";
 export * from "./table.module";
 export * from "./table.types";
-export * from "./table-url.service";
-export * from "./store/url-state/actions";
-export * from "./store/url-state/selectors";
