b7934103471c4180cef3828db6a56c1fa8236c52
Salvatore Niglio <salvatore.niglio@inobeta.net>
2026-08-06 16:24:13 +0200

    DEVK-908 docs(storybook) translations setup on prod builds

--- .storybook/i18n.ts
+++ .storybook/i18n.ts
@@ -1,19 +1,43 @@
-import { HttpClient } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import {
   TranslateLoader,
   TranslateModule,
   TranslateService,
 } from "@ngx-translate/core";
-import { IbTranslateModuleLoader } from "../src/app/inobeta-ui/translate/translate-loader.service";
+import { of } from "rxjs";
+import translations from "../src/assets/i18n/it.json";
+import { ibHttpTranslations } from "../src/app/inobeta-ui/http/translations";
+import { ibFormTranslations } from "../src/app/inobeta-ui/ui/forms-utilities/translations";
+import { ibKaiFilterTranslations } from "../src/app/inobeta-ui/ui/kai-filter/translations";
+import { ibKaiTableTranslations } from "../src/app/inobeta-ui/ui/kai-table/translations";
+import { ibMaterialFormTranslations } from "../src/app/inobeta-ui/ui/material-forms/translations";
+import { ibModalTranslations } from "../src/app/inobeta-ui/ui/modal/translations";
+import { ibViewTranslations } from "../src/app/inobeta-ui/ui/views/translations";
+
+const storybookTranslations = {
+  ...translations,
+  shared: {
+    ...ibHttpTranslations.it,
+    ...ibMaterialFormTranslations.it,
+    ...ibKaiFilterTranslations.it,
+    ...ibViewTranslations.it,
+    ...ibKaiTableTranslations.it,
+    ...ibModalTranslations.it,
+    ...ibFormTranslations.it,
+    ...translations.shared,
+  },
+};
+
+const storybookTranslateLoader: TranslateLoader = {
+  getTranslation: () => of(storybookTranslations),
+};
 
 @NgModule({
   imports: [
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
-        useExisting: IbTranslateModuleLoader,
-        deps: [HttpClient],
+        useValue: storybookTranslateLoader,
       },
     }),
   ],
