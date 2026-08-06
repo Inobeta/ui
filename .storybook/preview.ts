6058c771b1f827601a1548d601ab17d3e8455eb1
Salvatore Niglio <salvatore.niglio@inobeta.net>
2026-08-06 16:09:11 +0200

    DEVK-908 docs(storybook) documentation style back to work

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,6 +2,9 @@ import { HttpClientModule } from "@angular/common/http";
 import { importProvidersFrom } from "@angular/core";
 import { applicationConfig, type Preview } from "@storybook/angular-vite";
 import { version } from '../package.json'
+import '../src/styles.css';
+import '../src/app/examples/theme.scss';
+
 const [major, minor, patch] = version.split('.')
 
 const preview: Preview = {
